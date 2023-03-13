73c8dd11266d3e54df842509074c9efb38060c16
TooFaeded <45880087+TooFaeded@users.noreply.github.com>
2023-03-13 04:25:11 -0400

    Update pan.js

--- pan.js
+++ pan.js
@@ -2,8 +2,6 @@ const track = document.getElementById("image-track");
 
 const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
 
-console.log(track.dataset)
-
 const handleOnUp = () => {
   track.dataset.mouseDownAt = "0";  
   track.dataset.prevPercentage = track.dataset.percentage;
