b05cf0bc1462c093e6f6a4357b259e8cee5b1f8e
TooFaeded <jordan.lanctot@ryerson.ca>
2023-03-18 15:50:52 -0400

    Fix code breaking issues

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,38 +1,38 @@
 /* -- Parallax movement of the portfolio items -- */
-const track = document.getElementById("img-track");
-let prevPct = 0;
+const track = document.getElementById("image-track");
+let prevPercentage = 0;
 
-const handleOnDwn = e => {
-    track.dataset.mouseDwnAt = e.clientX;
+const handleOnDown = e => {
+    track.dataset.mouseDownAt = e.clientX;
 };
 
 const handleOnUp = () => {
-    track.dataset.mouseDwnAt = "0";
-    track.dataset.prevPct = track.dataset.pct;
+    track.dataset.mouseDownAt = "0";
+    track.dataset.prevPercentage = track.dataset.percentage;
 };
 
 const handleOnMove = e => {
-    if (track.dataset.mouseDwnAt === "0") return;
+    if (track.dataset.mouseDownAt === "0") return;
 
     const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
     const maxDelta = window.innerWidth * 3 / 5;
-    const pct = (mouseDelta / maxDelta) * -100;
-    const nextPctUnconstrained = parseFloat(track.dataset.prevPct) + pct;
-    const nextPct = Math.max(Math.min(nextPctUnconstrained, 0), -100);
+    const percentage = (mouseDelta / maxDelta) * -100;
+    const nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage;
+    const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
 
-    track.dataset.pct = nextPct;
+    track.dataset.percentage = nextPercentage;
 
     track.animate(
         {
-            transform: `translate(${nextPct}%, -50%)`,
+            transform: `translate(${nextPercentage}%, -50%)`,
         },
         { duration: 1400, fill: "forwards" }
     );
 
-    for (const img of track.getElementsByClassName("track-img")) {
-        img.animate(
+    for (const image of track.getElementsByClassName("track-image")) {
+        image.animate(
             {
-                objectPosition: `${100 + nextPct}% center`,
+                objectPosition: `${100 + nextPercentage}% center`,
             },
             { duration: 1400, fill: "forwards" }
         );
@@ -40,40 +40,40 @@ const handleOnMove = e => {
 };
 
 /* -- Handling a button to toggle the portfolio as visible or not -- */
-const pfBtn = document.getElementById("pf-btn");
-const pfCloseBtn = document.getElementById("pf-close-btn");
-const hdrPfBtn = document.getElementById("hdr-pf-btn");
+const portfolioBtn = document.getElementById("portfolio-btn");
+const portfolioCloseBtn = document.getElementById("portfolio-close-btn");
+const headerPortfolioBtn = document.getElementById("header-portfolio-btn");
 
-const tglTrackVis = () => {
+const toggleTrackVisibility = () => {
     const isTrackVisible = track.style.visibility === "visible";
-    const pct = isTrackVisible ? 0 : prevPct;
+    const percentage = isTrackVisible ? 0 : prevPercentage;
 
     track.style.visibility = isTrackVisible ? "hidden" : "visible";
-    track.dataset.mouseDwnAt = 0;
-    track.dataset.pct = pct;
-    track.dataset.prevPct = pct;
+    track.dataset.mouseDownAt = 0;
+    track.dataset.percentage = percentage;
+    track.dataset.prevPercentage = percentage;
 };
 
-const tglHdrBtnStyle = () => {
-    hdrPfBtn.classList.toggle("active");
+const toggleHeaderBtnStyle = () => {
+    headerPortfolioBtn.classList.toggle("active");
 
-    if (hdrPfBtn.classList.contains("active")) {
-        hdrPfBtn.style.backgroundColor = "#FFFAFF";
-        hdrPfBtn.style.color = "#0D1F22";
+    if (headerPortfolioBtn.classList.contains("active")) {
+        headerPortfolioBtn.style.backgroundColor = "#FFFAFF";
+        headerPortfolioBtn.style.color = "#0D1F22";
     } else {
-        hdrPfBtn.style.backgroundColor = "";
-        hdrPfBtn.style.color = "";
+        headerPortfolioBtn.style.backgroundColor = "";
+        headerPortfolioBtn.style.color = "";
     }
 };
 
-const tglVisAndStyle = () => {
-    tglTrackVis();
-    tglHdrBtnStyle();
+const toggleVisibilityAndStyle = () => {
+    toggleTrackVisibility();
+    toggleHeaderBtnStyle();
 };
 
-pfBtn.addEventListener("click", tglVisAndStyle);
-pfCloseBtn.addEventListener("click", tglVisAndStyle);
-hdrPfBtn.addEventListener("click", tglVisAndStyle);
+portfolioBtn.addEventListener("click", toggleVisibilityAndStyle);
+portfolioCloseBtn.addEventListener("click", toggleVisibilityAndStyle);
+headerPortfolioBtn.addEventListener("click", toggleVisibilityAndStyle);
 
 /* -- Add transition to slide in -- */
 const cards = document.querySelectorAll('.card');
@@ -85,19 +85,19 @@ cards.forEach((card, index) => {
 });
 
 /* -- Add accordion style toggling -- */
-const tglbtns = document.querySelectorAll('.tgl-btn');
+const toggleBtns = document.querySelectorAll('.toggle-btn');
 
-tglbtns.forEach(btn => {
+toggleBtns.forEach(btn => {
     btn.addEventListener('click', () => {
         const content = btn.nextElementSibling;
-        content.classList.tgl('active');
+        content.classList.toggle('active');
         btn.textContent = content.classList.contains('active') ? 'Show More' : 'Show Less';
     });
 });
 
 /* -- Add event listeners for touch events -- */
-window.addEventListener('mousedown', handleOnDwn);
-window.addEventListener('touchstart', e => handleOnDwn(e.touches[0]));
+window.addEventListener('mousedown', handleOnDown);
+window.addEventListener('touchstart', e => handleOnDown(e.touches[0]));
 window.addEventListener('mouseup', handleOnUp);
 window.addEventListener('touchend', e => handleOnUp(e.touches[0]));
 window.addEventListener('mousemove', handleOnMove);
